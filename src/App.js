5c201f3520402f592fd3f3ccf904bda69aac0b2f
Preil <preil2000@gmail.com>
2020-08-10 17:27:29 +0300

    Revert "OnClick method handler. Modifying state properties via setState."
    This reverts commit c00297a

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-    const [personsState, setPersonsState] = useState({
+class App extends Component {
+    state = {
         persons: [
             {name: 'Ilya', age: 39},
             {name: 'Alex', age: 38},
-            {name: 'Ilya', age: 37},
-        ],
-        // otherState: 'Some other value'
+            {name: 'Lena', age: 38}
+        ]
+    }
 
-    });
-
-    // Notice that switchNameHandler changes state,
-    // but it erases otherState while setPersonState applied.
-    // Does not merge the old state and new state
-
-    //better way is to use several useState:
-    const [otherState, setOtherState] = useState('Some other value');
-
-    console.log(personsState, otherState);
-
-    const switchNameHandler = () => {
-        setPersonsState({
+    switchNameHandler = ()=>{
+        // console.log('Was clicked')
+        this.setState({
             persons: [
-                {name: 'Ilya', age: 29},
-                {name: 'Alex', age: 28},
-                {name: 'Ilya', age: 27},
+                {name: 'Ilya', age: 39},
+                {name: 'Alex', age: 38},
+                {name: 'Lena', age: 33}
+
             ]
         })
     };
 
-    return (
-        <div className="App">
-            <h1>Hi,I'm a React App</h1>
-            <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
-                My hobby is cars.
-            </Person>
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-        </div>
-    );
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi,I'm a React App</h1>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+                    My hobby is cars.
+                </Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                         </div>
+        );
+    }
 }
 
-export default app;
+export default App;
