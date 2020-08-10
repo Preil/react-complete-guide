c00297ac24529ec44c0a1157dba937a49cb1ffa4
Preil <preil2000@gmail.com>
2020-08-10 08:07:51 +0300

    OnClick method handler. Modifying state properties via setState.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi,I'm a React App</h1>
-          <Person name="Ilya" age="39" />
-          <Person name="Alex" age="38" > My hobbies: Cars</Person>
-          <Person name="Lena" age="38" />
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            {name: 'Ilya', age: 39},
+            {name: 'Alex', age: 38},
+            {name: 'Lena', age: 38}
+        ]
+    }
+
+    switchNameHandler = ()=>{
+        // console.log('Was clicked')
+        this.setState({
+            persons: [
+                {name: 'Ilya', age: 39},
+                {name: 'Alex', age: 38},
+                {name: 'Lena', age: 33}
+
+            ]
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi,I'm a React App</h1>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+                    My hobby is cars.
+                </Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                         </div>
+        );
+    }
 }
 
 export default App;
