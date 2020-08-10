fdfa0eee133a1794982be68694164673f473026c
Preil <preil2000@gmail.com>
2020-08-10 19:49:42 +0300

    Passing arguments to child component. Two variants.

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
         ]
     }
 
-    switchNameHandler = ()=>{
+    switchNameHandler = (newName)=>{
         // console.log('Was clicked')
         this.setState({
             persons: [
-                {name: 'Ilya', age: 39},
+                {name: newName, age: 39},
                 {name: 'Alex', age: 38},
                 {name: 'Lena', age: 33}
 
@@ -27,14 +27,16 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hi,I'm a React App</h1>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
+                // this approach is more efficient
+                <button onClick={this.switchNameHandler.bind(this, 'Denis')}>Switch Name</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}/>
                 <Person
                     name={this.state.persons[1].name}
                     age={this.state.persons[1].age}
-                    click={this.switchNameHandler} >My hobby is cars.
+                    //this approach is less efficient
+                    click={()=>this.switchNameHandler('Ilya')} >My hobby is cars.
                 </Person>
                 <Person
                     name={this.state.persons[2].name}
