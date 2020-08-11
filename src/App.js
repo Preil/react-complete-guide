9601b2d5929d81cf32216240b4f0fbfeffca1f43
Preil <preil2000@gmail.com>
2020-08-11 15:37:46 +0300

    Flexible lists. Operating with state arrays. Without direct mutating the state.

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person'
 class App extends Component {
     state = {
         persons: [
-            {name: 'Ilya', age: 39},
-            {name: 'Lena', age: 38},
-            {name: 'Alex', age: 39}
+            {id: '1wef' ,name: 'Ilya', age: 39},
+            {id: 'sdf1' ,name: 'Lena', age: 38},
+            {id: '1sdf' ,name: 'Alex', age: 39}
         ],
         showPersons: false
     };
@@ -23,6 +23,26 @@ class App extends Component {
         persons.splice(personIndex, 1);
         this.setState({persons: persons})
     };
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(p=>{
+            return p.id === id;
+        });
+
+        const person = {
+            ...this.state.persons[personIndex]
+        };
+
+        //Alternative:
+        // const person_alterantive = Object.assign({}, this.state.persons[personIndex]);
+
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({persons: persons});
+
+    };
 
     render() {
         let persons = null;
@@ -35,7 +55,8 @@ class App extends Component {
                                 name={person.name}
                                 age={person.age}
                                 click={() => this.deletePersonHandler(index)}
-                                key={index}/>
+                                key={person.id}
+                                changed={(event) => this.nameChangedHandler(event, person.id)}/>
                         }
                     )}
                 </div>
