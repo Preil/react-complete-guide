2ee3aa0e10aeaddbfe9a4e7aba14117871fb35da
Preil <preil2000@gmail.com>
2020-08-11 12:08:35 +0300

    Show content dynamically example.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, {Component} from 'react';
 import './App.css';
-
-import UserInput from './UserInput/UserInput'
-import UserOutput from './UserOutput/UserOutput'
+import Person from './Person/Person'
 
 class App extends Component {
-    state ={
-        username: "Lena"
+    state = {
+        persons: [
+            {name: 'Ilya', age: 39},
+            {name: 'Lena', age: 38},
+            {name: 'Alex', age: 39}
+        ],
+        showPersons: false
     };
 
-    userNameChanged = (event) =>{
-        this.setState({username: event.target.value})
-    };
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        this.setState({showPersons: !doesShow})
+    }
+
     render() {
         return (
             <div className="App">
-                <UserInput
-                    changed={this.userNameChanged}
-                    currentName={this.state.username} />
-                <UserOutput userName='Ilya'/>
-                <UserOutput userName={this.state.username}/>
-                <UserOutput/>
+                <button onClick={this.togglePersonsHandler}>Show persons</button>
+                {this.state.showPersons ?
+                    <div>
+                        <Person
+                            name={this.state.persons[0].name}
+                            age={this.state.persons[0].age}/>
+                        <Person
+                            name={this.state.persons[1].name}
+                            age={this.state.persons[1].age}/>
+                        <Person
+                            name={this.state.persons[2].name}
+                            age={this.state.persons[2].age}/>
+                    </div> : null
+                }
             </div>
         )
     }
