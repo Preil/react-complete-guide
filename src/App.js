7f5ca72efa2159e7fbde9c37f5c7a401e1779095
Preil <preil2000@gmail.com>
2020-08-12 21:19:06 +0300

    Radium added. Some examples.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
+import Radium from 'radium'
+
 import Person from './Person/Person'
 import ValidationComponent from './ValidationComponent/ValidationComponent'
 import Char from './Char/Char'
+
 
 class App extends Component {
     state = {
@@ -67,7 +70,11 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
         };
 
         let persons = null;
@@ -87,6 +94,10 @@ class App extends Component {
                 </div>
             )
             style.backgroundColor='red'
+            style[':hover']={
+                backgroundColor: 'salmon',
+                color: 'black'
+            }
         }
 
         const classes=[];
@@ -118,4 +129,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
