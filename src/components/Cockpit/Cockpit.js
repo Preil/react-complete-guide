5f32b67d5703ec145f6343caeffa42d7bac0c931
Preil <preil2000@gmail.com>
2020-08-20 18:58:17 +0300

    Creating reference in functional components.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,60 +1,51 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
 import classes from './Cockpit.css'
 
 
 const cockpit = (props) => {
 
-        useEffect(() => {
-            // This hook executes every state update and every render() cycle
-            console.log('[Cockpit.js] useEffect')
-            // Can make http request here if needed.
-            const timer = setTimeout(() => {
-                alert('Saved data to cloud');
-            }, 1000);
+    const toggleBtnRef = useRef(null);
 
-            // we can add return to have clean up work? after component unmount action
-            return () => {
-                clearTimeout(timer);
-                console.log('[Cockpit.js] cleanup work in useEffect')
-            }
+    useEffect(() => {
+        toggleBtnRef.current.click();
+        console.log('[Cockpit.js] useEffect')
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect')
+        }
+    }, []);
 
-            // this will start useEffect after persons were changed
-        }, []);
 
-        // we can apply useEffect() for different object changes as mny times as needed
-
-        useEffect(() => {
-            console.log('[Cockpit.js] 2nd useEffect');
-            return() =>{
-                console.log('[Cockpit.js] Clean up work in 2nd useEffect');
-            }
-
-        });
-
-        const assignedClasses = [];
-
-        let btnClass = '';
-        if (props.showPersons) {
-            btnClass = classes.Red;
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect');
+        return () => {
+            console.log('[Cockpit.js] Clean up work in 2nd useEffect');
         }
 
-        if (props.personsLength <= 2) {
-            assignedClasses.push(classes.red); // classes = ['red']
-        }
-        if (props.personsLength <= 1) {
-            assignedClasses.push(classes.bold) // classes = ['red, 'bold']
-        }
+    });
 
-        return (
-            <div className={classes.Cockpit}>
-                <h1>{props.title}</h1>
-                <p>{props.personsLength}</p>
-                <p className={assignedClasses.join(' ')}>This is really working</p>
-                <button className={btnClass} onClick={props.clicked}>Toggle persons</button>
+    const assignedClasses = [];
 
-            </div>
-        );
+    let btnClass = '';
+    if (props.showPersons) {
+        btnClass = classes.Red;
     }
-;
+
+    if (props.personsLength <= 2) {
+        assignedClasses.push(classes.red); // classes = ['red']
+    }
+    if (props.personsLength <= 1) {
+        assignedClasses.push(classes.bold) // classes = ['red, 'bold']
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1>{props.title}</h1>
+            <p>{props.personsLength}</p>
+            <p className={assignedClasses.join(' ')}>This is really working</p>
+            <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>Toggle persons</button>
+
+        </div>
+    );
+};
 
 export default React.memo(cockpit);
